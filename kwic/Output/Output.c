00b70e9a5a6ed4a3ec25e21bf85d944468674c06
John Edward Tribe <tribeje@cub15.csci.viu.ca>
2013-11-25 16:59:40 -0800

    modified the output to unshift words and underline keyword

--- kwic/Output/Output.c
+++ kwic/Output/Output.c
@@ -18,23 +18,61 @@ void OUInit(void)
 
 void OUPrint(void)
 {
-	int i,j;
-
+        int i,j,s;
+	int keyWordIndex;
+	int keyWordLength;
+	int keyWordFound;
 	for (i = 0; i < SSNumLines(); i++) {
 		int numWords = SSNumWords(i);
+		const char* keyWord = SSGetWord(i,0);
+		keyWordIndex = 0;
+		keyWordFound = 0;
+		keyWordLength = 0;
 		for (j = 0; j < numWords; j++) {
 			int shiftNum = SSGetShiftNum(i);
-			printf("%s",SSGetWord(i,j));
 
-			/* no comma if line is unshifted
-			 * otherwise comma follows last word on original line
-			 */
-			if (shiftNum != 0 && j == numWords-shiftNum-1)
-				printf(",");
+			/* reverse word shifting */
+			const char* curWord;
+			if (j < shiftNum)
+			{
+				curWord = SSGetWord(i,numWords-shiftNum+j);
+				printf("%s", curWord);
+			}
+			else
+			{
+				curWord = SSGetWord(i,j-shiftNum);
+				printf("%s",SSGetWord(i,j-shiftNum));
+			}
+			
+			/* determine underlining index and length */
+			if (curWord == keyWord)
+			{
+			        keyWordFound = 1;
+				const char *s;
+				for (s = curWord; *s; ++s);
+				keyWordLength += (s - curWord);  
+			}
+			else if(!keyWordFound)
+			{
+			        const char *s;
+			        for (s = curWord; *s; ++s);
+			        keyWordIndex += (s - curWord)+1;  
+			}
 
 			/* space follows every word but the last */
 			if (j != SSNumWords(i)-1)
 				printf(" ");
+		}
+		printf("\n");
+
+		/* underline keyWord */
+		for(j = 0; j < keyWordIndex; j++)
+		{
+		        printf (" ");
+		}
+		for (j = 0; j < keyWordLength; j++)
+		{ 
+		        printf("-");
 		}
 		printf("\n");
 	}
